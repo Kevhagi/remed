2cae68a6f5460b044e2fdc384e411234719324b7
Kevin Jaya Susilo <kevinjayasusilo@kevins-MacBook-Air.local>
2022-05-29 13:26:17 +0700

    Base merging project & latihan octa

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,8 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','PagesController@homepage');
+Route::get('about','PagesController@about');
+Route::get('siswa','SiswaController@index');
+Route::get('siswa/create', 'SiswaController@create');
+Route::get('siswa/create','SiswaController@create');
+Route::post('siswa','SiswaController@store');
+Route::get('siswa/{siswa}','SiswaController@show');
